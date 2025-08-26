2e7af11e937b53322f0d55d06721b7992b8d1fff
Mizanit8582 <mijan1981bmda@gmail.com>
2025-08-26 23:28:07 +0600

    dart-01
    Dart programing start

---
+++ Dart_class/start_dart.dart
@@ -0,0 +1,35 @@
+import 'dart:ffi';
+
+void main(){
+  print('This is my first project');
+//   Dart variable
+ int x = 50;
+ int y = 50;
+ int result = x+y;
+ print(result);
+//  Data type in dart এখানে আমরা বিভিন্ন ধরনের ডাটা টাইপ সম্পকর্ে জানলাম
+/*Int l = 20;
+Double p = 20.21;
+String z = "mijan";
+bool = true/false;*/
+List Mylist = ['aman', 'jaman',1,2,];
+Map parson= {
+  'name': 'Mijan',
+  'Age' : 52,
+  'country': 'bangladesh'
+};
+// টাইপ ইনফারেন্স var এবং  dynamic এর মাধ্যমে
+var f= 52;
+var h = 52.20;
+var d = 'Mijan'
+  /*এখানে var লেখার মাধ্যমে টাইপ ইনফারেন্স করা হয়।
+  কারন var লিখলে ইনটেলিজ আইডিয়া বুঝে নিবে িএটা কোন ধরনের Data type.*/
+  // const and final
+  /*ডাটা পরিবর্ন না করতে চাইলে var এর বদলে
+  const and final keyword ব্যবহার করতে হয়।*/
+
+ /*এরিথমেটিক অপারেটর*/
+ /* যোগ, বিয়োগ, গুন, ভাগ, ভাগশেষ ইত্যাদিতে এই অপারেটর ব্যবহার হয়।
+ +,-,*,/
+ */
+}
